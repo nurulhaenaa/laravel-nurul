815a960c3ef085f5c61e51f46ac38e9566ce98b5
nurulhaenaa <128347817+nurulhaenaa@users.noreply.github.com>
2024-02-04 22:08:36 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('masyarakats', function (Blueprint $table) {
+            $table->char('nik', 16)->primary();
+            $table->string('nama', 35);
+            $table->string('username', 25);
+            $table->string('password', 32);
+            $table->string('telp', 13);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('masyarakats');
     }
 };
