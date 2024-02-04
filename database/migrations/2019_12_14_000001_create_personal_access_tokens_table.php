fa3e63e009a846217fc3f92f1ca676fa21f1ddb8
nurulhaenaa <128347817+nurulhaenaa@users.noreply.github.com>
2024-02-04 22:49:26 +0700

    Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('petugases', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('nama_petugas', 35);
+            $table->string('username', 25);
+            $table->string('password', 32);
+            $table->string('telp', 13);
+            $table->enum('level', ['admin', 'petugas']);
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('petugases');
     }
 };
