de4544670ba87e0a3d42ccc98c73996310503abb
nurulhaenaa <128347817+nurulhaenaa@users.noreply.github.com>
2024-02-04 22:48:43 +0700

    Update 2019_08_19_000000_create_failed_jobs_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('tanggapans', function (Blueprint $table) {
+            $table->id('id_tanggapan');
+            $table->foreignId('id_pengaduans')->references('id')->on('pengaduans');
+            $table->date('tanggal_tanggapan');
+            $table->text('tanggapan');
+            $table->foreignId('id_petugass')->references('id')->on('petugases');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tanggapans');
     }
 };
